2b85f3fbeb78ead397c047fab683bca8bec11306
StarTux <ths.loy@googlemail.com>
2022-02-12 13:45:43 +0100

    Implement remote command API

--- src/main/java/com/winthier/connect/CoreConnect.java
+++ src/main/java/com/winthier/connect/CoreConnect.java
@@ -1,8 +1,16 @@
 package com.winthier.connect;
 
+import com.winthier.connect.message.RemotePlayerCommandMessage;
+import org.bukkit.entity.Player;
+
 public final class CoreConnect implements com.cavetale.core.connect.Connect {
     @Override
     public String getServerName() {
         return ConnectPlugin.instance.connect.getServerName();
+    }
+
+    @Override
+    public void dispatchRemoteCommand(Player player, String command, String targetServer) {
+        new RemotePlayerCommandMessage(player, command).send(targetServer);
     }
 }
