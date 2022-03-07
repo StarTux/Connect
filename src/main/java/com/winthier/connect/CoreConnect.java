01d1b64bf0cb6fc31c906673475d5690acc8020c
StarTux <ths.loy@googlemail.com>
2022-03-07 15:41:08 +0100

    Implement the Core Connect API

--- src/main/java/com/winthier/connect/CoreConnect.java
+++ src/main/java/com/winthier/connect/CoreConnect.java
@@ -1,6 +1,10 @@
 package com.winthier.connect;
 
 import com.winthier.connect.message.RemotePlayerCommandMessage;
+import com.winthier.connect.payload.OnlinePlayer;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
 import org.bukkit.entity.Player;
 
 public final class CoreConnect implements com.cavetale.core.connect.Connect {
@@ -12,5 +16,14 @@ public final class CoreConnect implements com.cavetale.core.connect.Connect {
     @Override
     public void dispatchRemoteCommand(Player player, String command, String targetServer) {
         new RemotePlayerCommandMessage(player, command).send(targetServer);
+    }
+
+    @Override
+    public Set<UUID> getOnlinePlayers() {
+        Set<UUID> result = new HashSet<>();
+        for (OnlinePlayer player : Connect.getInstance().getOnlinePlayers()) {
+            result.add(player.getUuid());
+        }
+        return result;
     }
 }
