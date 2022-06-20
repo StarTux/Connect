069002b09fef3ba66737d97cc3ec84f3c2cf70d5
StarTux <ths.loy@googlemail.com>
2022-06-20 12:23:20 +0200

    Use async threads and cached values

--- src/main/java/com/winthier/connect/CoreConnect.java
+++ src/main/java/com/winthier/connect/CoreConnect.java
@@ -33,59 +33,76 @@ public final class CoreConnect implements com.cavetale.core.connect.Connect {
 
     @Override
     public void sendMessage(String targetServer, String channel, String payload) {
-        plugin.connect.send(targetServer, channel, payload);
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
+                plugin.connect.send(targetServer, channel, payload);
+            });
     }
 
     @Override
     public void broadcastMessage(String channel, String payload) {
-        plugin.connect.broadcast(channel, payload);
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
+                plugin.connect.broadcast(channel, payload);
+            });
     }
 
     @Override
     public void broadcastMessage(ServerGroup group, String channel, String payload) {
         NetworkServer thisServer = NetworkServer.current();
-        for (String server : plugin.connect.listServers()) {
-            NetworkServer thatServer = NetworkServer.of(server);
-            if (thisServer != thatServer && thatServer.group == group) {
-                plugin.connect.send(server, channel, payload);
-            }
-        }
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
+                for (String server : plugin.connect.listServers()) {
+                    NetworkServer thatServer = NetworkServer.of(server);
+                    if (thisServer != thatServer && thatServer.group == group) {
+                        plugin.connect.send(server, channel, payload);
+                    }
+                }
+            });
     }
 
     @Override
     public void broadcastMessageToAll(String channel, String payload) {
-        plugin.connect.broadcastAll(channel, payload);
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
+                plugin.connect.broadcastAll(channel, payload);
+            });
     }
 
     @Override
     public void broadcastMessageToAll(ServerGroup group, String channel, String payload) {
-        for (String server : plugin.connect.listServers()) {
-            NetworkServer thatServer = NetworkServer.of(server);
-            if (thatServer.group == group) {
-                plugin.connect.send(server, channel, payload);
-            }
-        }
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
+                for (String server : plugin.connect.listServers()) {
+                    NetworkServer thatServer = NetworkServer.of(server);
+                    if (thatServer.group == group) {
+                        plugin.connect.send(server, channel, payload);
+                    }
+                }
+            });
     }
 
     @Override
     public void dispatchRemoteCommand(Player player, String command, String targetServer) {
         RemotePlayerCommandMessage message = new RemotePlayerCommandMessage(player, command);
-        if (plugin.connect.listServers().contains(targetServer)) {
-            message.send(targetServer);
-        } else {
-            plugin.pendingRemoteCommandMap.computeIfAbsent(targetServer, t -> new HashMap<>())
-                .put(player.getUniqueId(), message);
-            Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
+                if (plugin.connect.listServers().contains(targetServer)) {
+                    message.send(targetServer);
+                } else {
                     Redis.lpush("cavetale.server_wake." + targetServer, "wake_up", 30L);
-                });
-            player.sendMessage(text("Please wait while server is starting up...", YELLOW));
-        }
+                    Bukkit.getScheduler().runTask(plugin, () -> {
+                            plugin.pendingRemoteCommandMap.computeIfAbsent(targetServer, t -> new HashMap<>())
+                                .put(player.getUniqueId(), message);
+                            player.sendMessage(text("Please wait while server is starting up...", YELLOW));
+                        });
+                }
+            });
+    }
+
+    @Override
+    public int getOnlinePlayerCount() {
+        return plugin.connect.cachedPlayerCount;
     }
 
     @Override
     public Set<UUID> getOnlinePlayers() {
         Set<UUID> result = new HashSet<>();
-        for (OnlinePlayer player : Connect.getInstance().getOnlinePlayers()) {
+        for (OnlinePlayer player : plugin.connect.cachedPlayerList) {
             result.add(player.getUuid());
         }
         return result;
@@ -94,24 +111,24 @@ public final class CoreConnect implements com.cavetale.core.connect.Connect {
     @Override
     public List<RemotePlayer> getRemotePlayers() {
         List<RemotePlayer> result = new ArrayList<>();
-        Connect.getInstance().listPlayers().forEach((server, list) -> {
-                for (OnlinePlayer online : list) {
-                    result.add(new ConnectRemotePlayer(online.getUuid(), online.getName(), server));
-                }
-            });
+        for (OnlinePlayer online : plugin.connect.cachedPlayerList) {
+            result.add(new ConnectRemotePlayer(online.getUuid(), online.getName(), online.getServer()));
+        }
         return result;
     }
 
     @Override
     public RemotePlayer getRemotePlayer(UUID uuid) {
-        OnlinePlayer player = plugin.connect.findOnlinePlayer(uuid);
-        return player != null
-            ? new ConnectRemotePlayer(player.getUuid(), player.getName(), player.getServer())
-            : null;
+        for (OnlinePlayer online : plugin.connect.cachedPlayerList) {
+            if (uuid.equals(online.getUuid())) {
+                return new ConnectRemotePlayer(online.getUuid(), online.getName(), online.getServer());
+            }
+        }
+        return null;
     }
 
     @Override
     public Set<String> getOnlineServerNames() {
-        return Set.copyOf(plugin.connect.listServers());
+        return Set.copyOf(plugin.connect.cachedPlayerMap.keySet());
     }
 }
